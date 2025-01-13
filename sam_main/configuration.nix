459a3a7aa85595a3dd8e5ed940e095737b3e9794
sreiswig <sam.reiswig@gmail.com>
2025-01-12 20:43:41 -0600

    split config

--- sam_main/configuration.nix
+++ sam_main/configuration.nix
@@ -9,7 +9,8 @@
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
       ./docker.nix
-      # GPU config in file./nvidia_gpu.nix
+      # GPU config in file
+      ./nvidia_gpu.nix
     ];
 
   # Bootloader.
@@ -84,40 +85,6 @@
 
   # Enable touchpad support (enabled default in most desktopManager).
   # services.xserver.libinput.enable = true;
-
-  # Load nvidia driver for Xorg and Wayland
-  services.xserver.videoDrivers = ["nvidia"];
-
-  hardware.nvidia = {
-    # Modesetting is required.
-    modesetting.enable = true;
-    
-    # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
-    # Enable this if you have graphical corruption issues or application crashes after waking
-    # up from sleep. This fixes it by saving the entire VRAM memory to /tmp/ instead
-    # of just the bare essentials.
-    powerManagement.enable = false;
-
-    # Fine-grained power management. Turns off GPU when not in use.
-    # Experimental and only works on modern Nvidia GPUs (Turing or newer).
-    powerManagement.finegrained = false;
-    
-    # Use the NVidia open source kernel module (not to be confused with the 
-    # independent third-party "nouveau" open source driver).
-    # Support is limited to the Turing and later architectures. Full list of 
-    # supported GPUs is at:
-    # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus
-    # Only available from driver 515.43.04+
-    # Currently alpha-quality/buggy, so false is currently recommended setting.
-    open = false;
-
-    # Enable the Nvidia settings menu,
-    # accessible via 'nvidia-settings'.
-    nvidiaSettings = true;
-
-    # Optionally, you may need to select the appropriate driver version for your specific GPU.
-    package = config.boot.kernelPackages.nvidiaPackages.stable;
-  };
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.sam = {
