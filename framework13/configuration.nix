3b6806af655453cffe57513f962e7841c58ef16d
sam <sam.reiswig@gmail.com>
2025-01-25 22:30:44 -0600

    update laptop

--- framework13/configuration.nix
+++ framework13/configuration.nix
@@ -14,7 +14,7 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  networking.hostName = "nixos"; # Define your hostname.
+  networking.hostName = "framework13"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
   # Configure network proxy if necessary
@@ -100,9 +100,7 @@
     description = "Sam";
     extraGroups = [ "networkmanager" "wheel" "docker" ];
     packages = with pkgs; [
-      firefox
       google-chrome
-    #  thunderbird
     ];
   };
 
