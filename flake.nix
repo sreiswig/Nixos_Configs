1e70660c9e4d6e25044c780a06a465fd61936afe
sreiswig <sam.reiswig@gmail.com>
2025-01-26 22:21:06 -0600

    starting to flake

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = "Main Flake for Main, Laptop, AI Server configuration";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs, ... }@inputs: {
+    nixosConfigurations = {
+      # Main
+      sam-main = nixpkgs.lib.nixosSystem {
+        modules = [./hosts/sam_main];
+      };
+      # Laptop
+      framework13 = nixpkgs.lib.nixosSystem {
+        modules = [./hosts/framework13];
+      };
+      # Server
+      aiserver = nixpkgs.lib.nixosSystem {
+        modules = [./hosts/AIServer];
+      };
+    };
+  };
+}
