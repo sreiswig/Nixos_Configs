bebaa329af915151f71a837d31a9a07de211fa43
sreiswig <sam.reiswig@gmail.com>
2026-02-18 20:37:43 -0600

    add ceph

--- hosts/dad_nas/default.nix
+++ hosts/dad_nas/default.nix
@@ -4,8 +4,14 @@
   imports = [
     ./configuration.nix
     ../../modules/services/opentelemetry.nix
+    ../../modules/services/ceph.nix
   ];
 
   services.my-opentelemetry.enable = true;
   services.my-opentelemetry.role = "agent";
+
+  services.homelab-ceph = {
+    enable = true;
+    role = "server";
+  };
 }
--- hosts/sam_main/default.nix
+++ hosts/sam_main/default.nix
@@ -7,6 +7,7 @@
     ../../modules/desktop
     ../../modules/hardware/gpu/nvidia_gpu.nix
     ../../modules/services/opentelemetry.nix
+    ../../modules/services/ceph.nix
 
     # Local Configuration
     ./hardware-configuration.nix
@@ -15,6 +16,11 @@
 
   services.my-opentelemetry.enable = true;
   services.my-opentelemetry.role = "agent";
+
+  services.homelab-ceph = {
+    enable = true;
+    role = "client";
+  };
 
   networking.hostName = "sam_nixos";
 
