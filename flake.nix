ceca5576fe9e5800fac04c6d90eaca4503e417f1
sam <sam.reiswig@gmail.com>
2026-02-20 20:45:59 -0600

    fixes the bashrc on framework13 flake

--- flake.nix
+++ flake.nix
@@ -29,6 +29,7 @@
             {
               home-manager.useGlobalPkgs = true;
               home-manager.useUserPackages = true;
+              home-manager.backupFileExtension = "backup";
               home-manager.users.sam = {
                 imports = [ 
                   ./modules/home-manager 
